206fd74d6f7ffb822d730aeb0cc5a8bf3ebea338
ORKA1 <85589926+ORKA1@users.noreply.github.com>
2022-04-20 04:16:31 +0200

    Add files via upload

---
+++ ORKA.py
@@ -0,0 +1,59 @@
+import os
+import time
+os.system("clear")
+print("""\033[0;92m _____   _____    _   _        ___  
+/  _  \ |  _  \  | | / /      /   | 
+| | | | | |_| |  | |/ /      / /| | 
+| | | | |  _  /  | |\ \     / / | | 
+| |_| | | | \ \  | | \ \   / /  | | 
+\_____/ |_|  \_\ |_|  \_\ /_/   |_|
+""")
+time.sleep(0.5)
+o=input ("Enter the password please >>")
+print("\033[0;97m")
+
+if o=="orka&hamo":
+    os.system("apt-get update")
+    os.system("apt upgrade")
+    os.system("apt update")
+    os.system("pkg install update")
+    os.system("pkg install upgrade")
+    os.system("termux-setup-storage")
+    os.system("apt-get update")
+    os.system("apt-get upgrade")
+    os.system("pkg install nmap")
+    os.system("pkg install netcat")
+    os.system("apt install php")
+    os.system("apt install python")
+    os.system("apt install python2")
+    os.system("apt install git")
+    os.system("apt install golang")
+    os.system("apt install nano")
+    os.system("apt install havij")
+    os.system("apt install cmatrix")
+    os.system("pkg install figlet")
+    os.system("pkg install wget")
+    os.system("pkg install wget -y")
+    os.system("pkg install python2-dev -y")
+    os.system("apt update && apt upgrade")
+    os.system("pip2 install requests")
+    os.system("pip2 install wget")
+    os.system("pkg install proot")
+    os.system("pkg install net-tools")
+    os.system("pkg install uzip")
+    os.system("pkg install tor")
+    os.system("pkg install openssh")
+    os.system("pkg install unzip")
+    os.system("pkg install curl")
+    os.system("pkg install ruby")
+    os.system("pkg install help ")
+    os.system("pkg install w3m")
+    print("\033[0;92mDownload successfully .. !")
+else:
+    print ("""\033[91m _____   _____    _____    _____   _____        
+| ____| |  _  \  |  _  \  /  _  \ |  _  \       
+| |__   | |_| |  | |_| |  | | | | | |_| |       
+|  __|  |  _  /  |  _  /  | | | | |  _  /       
+| |___  | | \ \  | | \ \  | |_| | | | \ \       
+|_____| |_|  \_\ |_|  \_\ \_____/ |_|  \_\
+    """)
